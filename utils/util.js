7bd82d25209c91c2f58b691b03c463e959991864
GanYihuan <583520052@qq.com>
2018-09-28 16:25:56 +0800

    bug

--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 const random = function generateMixed(n) {
@@ -19,28 +18,3 @@ module.exports = {
   random: random,
   breakSection: breakSection
 }
-=======
-const formatTime = date => {
-	const year = date.getFullYear()
-	const month = date.getMonth() + 1
-	const day = date.getDate()
-	const hour = date.getHours()
-	const minute = date.getMinutes()
-	const second = date.getSeconds()
-
-	return (
-		[year, month, day].map(formatNumber).join('/') +
-		' ' +
-		[hour, minute, second].map(formatNumber).join(':')
-	)
-}
-
-const formatNumber = n => {
-	n = n.toString()
-	return n[1] ? n : '0' + n
-}
-
-module.exports = {
-	formatTime: formatTime
-}
->>>>>>> dba8f6d840be923cba5963fa5fcec36d3f187230
